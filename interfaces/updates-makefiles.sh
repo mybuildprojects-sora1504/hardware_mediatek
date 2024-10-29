00118ba3b101aed23ff6a83d242c9ac6d63f2f89
rio004 <riyon336@gmail.com>
2024-10-29 01:16:56 +0000

    interfaces: Resolve duplication for AIDL/HIDL interfaces
    Change-Id: I0c8416ae4b9ff30fb0355b1643185ddb73744650
    Signed-off-by: rio004 <riyon336@gmail.com>

--- interfaces/updates-makefiles.sh
+++ interfaces/updates-makefiles.sh
@@ -3,7 +3,4 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.goodix:hardware/xiaomi/interfaces/goodix"
-
-do_makefiles_update \
-  "vendor.xiaomi:hardware/xiaomi/interfaces/xiaomi"
+  "vendor.mediatek.hardware:hardware/mediatek/interfaces"
